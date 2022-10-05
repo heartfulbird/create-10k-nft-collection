7e86453e5912306266f31bc9690cf258098834b3
stan <stassavickii@gmail.com>
2022-10-05 23:38:50 +0800

    txn status parsing fix

--- utils/functions/txnCheck.js
+++ utils/functions/txnCheck.js
@@ -3,6 +3,9 @@
 
 // ALERT about incomming connections fix:
 // https://github.com/puppeteer/puppeteer/issues/4752
+// Maybe you dont need the Certificate and anyway it requires login or admin pass and my own pass doesn't work even ifI'm an Admin...
+// try:
+// sudo codesign --force --sign - ./node_modules/puppeteer/.local-chromium/mac-961656/chrome-mac/Chromium.app --deep
 
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
@@ -26,10 +29,20 @@ async function txnCheck(url) {
     await page.waitForSelector("#ContentPlaceHolder1_maintable");
 
     try {
-      let cardText = await page.$eval("#ContentPlaceHolder1_maintable .row:nth-child(3) div:nth-child(2)", (text) => text.textContent);
+      // stopped working
+      // let cardText = await page.$eval("#ContentPlaceHolder1_maintable .row:nth-child(3) div:nth-child(2)", (text) => text.textContent);
+
+      // finds "Status:Success"
+      // let cardText = await page.$eval("#ContentPlaceHolder1_maintable .row:nth-child(4)", (text) => text.textContent);
+
+      // finds "Success"
+      let cardText = await page.$eval("#ContentPlaceHolder1_maintable .row:nth-child(4) div:nth-child(2)", (text) => text.textContent);
+
       await browser.close();
 
       console.log('Data found on a page:')
+      console.log(cardText)
+      console.log(cardText)
       console.log(cardText)
       console.log('check if it posted the data above')
 
